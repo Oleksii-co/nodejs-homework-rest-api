27c67fa3b1aac5e9bb25902013d185af42719e78
Oleksii <110523568+Oleksii-co@users.noreply.github.com>
2023-10-02 20:42:11 +0300

    done

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -4,6 +4,31 @@ import "dotenv/config";
 import bcrypt from "bcrypt";
 import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../helpers/index.js";
+import fs from "fs/promises";
+import path from "path";
+import gravatar from "gravatar";
+import Jimp from "jimp";
+
+const avatarPath = path.resolve("public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: oldPath, filename } = req.file;
+
+  const newPath = path.join(avatarPath, filename);
+
+  const updatedAvatar = await Jimp.read(oldPath);
+  updatedAvatar.resize(250, 250);
+  updatedAvatar.write(newPath);
+
+  await fs.unlink(oldPath);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -14,19 +39,33 @@ const register = async (req, res) => {
 
   const hashPass = await bcrypt.hash(password, 10);
 
-  const newUser = await User.create({ ...req.body, password: hashPass });
+  let avatarURL;
+
+  if (req.file) {
+    const { path: oldPath, filename } = req.file;
+    const newPath = path.join(avatarPath, filename);
+    await fs.rename(oldPath, newPath);
+    avatarURL = path.join("avatars", filename);
+  } else {
+    avatarURL = gravatar.url(email, { protocol: "https" });
+  }
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPass,
+    avatarURL,
+  });
 
   res.status(201).json({
     email: newUser.email,
     subscription: newUser.subscription,
+    avatarURL: newUser.avatarURL,
   });
 };
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-
   const user = await User.findOne({ email });
-
   const { JWT_SECRET } = process.env;
 
   if (!user) {
@@ -47,8 +86,8 @@ const login = async (req, res) => {
   res.status(200).json({
     token: token,
     user: {
-      email: result.email,
-      subscription: result.subscription,
+      email: user.email,
+      subscription: user.subscription,
     },
   });
 };
@@ -72,4 +111,5 @@ export default {
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
